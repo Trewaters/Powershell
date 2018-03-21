607e42f37f61cfbed8d530d3eee6c354b0e06c27
Tre' Grisby <trewaters@hotmail.com>
2018-03-21 06:38:39 -0700

    small changes I made to lab 8

--- Helpful scripts/scripts in draft/Corrupt_User_Profile.ps1
+++ Helpful scripts/scripts in draft/Corrupt_User_Profile.ps1
@@ -3,13 +3,78 @@
 
 # METHOD 1 Scripting of the KB
 # Prompt for username
+$winUser = Read-Host -Prompt 'Enter username'
+
+# testing I have the correct username
+#Write-Host "Fixing username $winUser."
+
 
 # profile folder in Registry ( HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList )
-# HELP ( https://blogs.technet.microsoft.com/heyscriptingguy/2011/11/10/use-powershell-to-find-user-profiles-on-a-computer/ )
+# HELP with Registry Keys (https://docs.microsoft.com/en-us/powershell/scripting/getting-started/cookbooks/working-with-registry-keys?view=powershell-6)
 
 $profilelist = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList'
-dir $profilelist
-Get-childItem 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' | % {Get-ItemProperty $_.pspath } | Select profileImagePath, sid
+# dir is an alias for 'Get-childItem'
+#dir $profilelist
+#dir $profilelist | format-List *
+
+# abbreviated 
+#Get-childItem 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' | % {Get-ItemProperty $_.pspath } | Select profileImagePath, sid
+# detailed
+Get-childItem 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' | ForEach-Object {Get-ItemProperty $_.pspath } | Select-Object profileImagePath, sid
+
+# another way to access the same information
+# abbreviated
+#gwmi win32_userprofile | select localpath, sid
+# detailed
+#Get-WmiObject win32_userprofile | Select-Object localpath,sid
+# how to get alias information for the commands used so far
+#Get-Alias gwmi, select, ft
+# show last time profile was used
+#gwmi win32_userprofile | select @{LABEL="last used";EXPRESSION={$_.ConvertToDateTime($_.lastusetime)}}, LocalPath, SID | ft -a
+
+#
+$path = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*'
+Get-ItemProperty -Path $path | Select-Object -Property PSChildName, ProfileImagePath
+#
+$objSID = New-Object System.Security.Principal.SecurityIdentifier("SID")
+$objUser = $objSID.Translate( [System.Security.Principal.NTAccount])
+$objUser.Value
+#
+$path = 'Registry::HKey_Local_Machine\Software\Microsoft\Windows NT\CurrentVersion\ProfileList\*'
+$items = Get-ItemProperty -path $path
+Foreach ($item in $items) {
+    $objUser = New-Object System.Security.Principal.SecurityIdentifier($item.PSChildName)
+    $objName = $objUser.Translate([System.Security.Principal.NTAccount])
+    $item.PSChildName = $objName.value
+}
+echo $items | Select-Object -Property PSChildName, ProfileImagePath
+
+# Get user profile path
+Get-ChildItem 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\ProfileList' | ForEach-Object { $_.GetValue('ProfileImagePath') }
+#ERROR - line below not working
+#Get-ChildItem 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\ProfileList' | ForEach-Object { $profilePath = $_.GetValue('ProfileImagePath') Get-ChildItem -Path "$profilePath\AppData\Local\Temp" }
+
+# windows user profile information
+get-CIMinstance -ClassName win32_UserProfile | select -first 1
+# extend to multiple machines like this
+Get-WmiObject -Class Win32_UserProfile -ComputerName 'bit01','CLIENT2'
+# HELP with $_ (https://technet.microsoft.com/en-us/library/ee677578.aspx)
+# HELP with methods (https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_methods?view=powershell-6)
+# HELP with split (https://blogs.technet.microsoft.com/heyscriptingguy/2014/07/17/using-the-split-method-in-powershell/)
+# Delete a user profile
+#Get-WmiObject -Class Win32_UserProfile | where {$_.LocalPath.split('')[-1] -eq 'Users.Beverly'} | foreach {$_.Delete()}
+
+Get-WmiObject -Class Win32_UserProfile -Property LocalPath 
+Get-WmiObject -Class Win32_UserProfile | % {$_.LocalPath.split('\') } 
+Get-WmiObject -Class Win32_UserProfile | where {$_.LocalPath.split('')[-1] -eq 'Users.Beverly'} | Out-String
+
+# HELP if (https://mcpmag.com/articles/2016/03/09/working-with-the-if-statement.aspx)
+# List user profiles that have not been used in 5 days
+Get-WMIObject -class Win32_UserProfile | Where {(!$_.Special) -and ($_.ConvertToDateTime($_.LastUseTime) -lt (Get-Date).AddDays(-5))}
+#Get-WMIObject -class Win32_UserProfile | % {($_.ConvertToDateTime($_.LastUseTime))}
+Get-WMIObject -class Win32_UserProfile | % {if($_.LastUseTime -gt 0){($_.ConvertToDateTime($_.LastUseTime))} }
+
+Get-WmiObject Win32_OperatingSystem | Get-Member -MemberType *method
 
 
 #my steps
--- Month of Lunches/Lab8.ps1
+++ Month of Lunches/Lab8.ps1
@@ -1,1 +1,2 @@
-﻿
+﻿# See all columns in process
+Get-Process | ConvertTo-HTML | Out-File "C:\Users\Tre'\Documents\Hunting\Coding Work Study\GitHub\Powershell\Month of Lunches\Labs\processes.html"
