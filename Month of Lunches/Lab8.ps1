b1cde84266f7a4cd9503ac3df9a1eb329dd912d9
Tre' Grisby <trewaters@hotmail.com>
2018-03-28 17:25:53 -0700

    a simple line about get-member

--- Month of Lunches/Lab8.ps1
+++ Month of Lunches/Lab8.ps1
@@ -1,2 +1,6 @@
 ﻿# See all columns in process
 Get-Process | ConvertTo-HTML | Out-File "C:\Users\Tre'\Documents\Hunting\Coding Work Study\GitHub\Powershell\Month of Lunches\Labs\processes.html"
+
+# Discovering object with Get-Member, aliased as `gm`
+Get-process | Get-Member
+Get-Process | gm
