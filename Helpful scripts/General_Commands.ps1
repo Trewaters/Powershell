89288b032f6e5b0e56a8481877a5142a0dedaeac
Tre' Grisby <trewaters@hotmail.com>
2018-03-30 07:37:08 -0700

    added a line about select-Object

--- Helpful scripts/General_Commands.ps1
+++ Helpful scripts/General_Commands.ps1
@@ -3,3 +3,9 @@ Format-Table
 
 # Formats list as a table
 Get-PSSnapin | Format-Table -Auto
+
+# Sort objects
+Get-Process | Sort-Object -property ProcessName -Descending
+
+# Select an object from the pipeline with `Select-Object`
+Get-Process | Select-Object -property Name,ID,VM,PM | ConvertTo-HTML | Out-File selectObject_Test2.html
