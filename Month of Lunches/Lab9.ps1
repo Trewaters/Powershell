d791c40e75fdf34c89162b6cd297e533bab6f9a5
Tre' Grisby <trewaters@hotmail.com>
2018-04-02 17:06:42 -0700

    working with Lab 9 today.

--- Month of Lunches/Lab9.ps1
+++ Month of Lunches/Lab9.ps1
@@ -3,18 +3,51 @@ Get-Content .\computer.txt | Get-Service
 
 Get-Help Get-Service -detailed
  
- # 1st pipe out command A => Get-Member
- Get-Content .\computer.txt | Get-Member
+# 1st pipe out command A => Get-Member
+Get-Content .\computer.txt | Get-Member
 
- # 2nd examine the full help of command B
- Get-Help Get-Service -Full
+# 2nd examine the full help of command B
+Get-Help Get-Service -Full
 
- #
- Get-Process -name note* | Stop-Process
- Get-Process -name chrome | 
+#
+Get-Process -name note* | Stop-Process
+Get-Process -name chrome | Stop-Process
 
- Get-Process | Get-Member
+Get-Process | Get-Member
 
- Get-help Stop-Process -full
+# 9.5
+Get-help Get-ADuser -full > "C:\Users\lgrisby\Downloads\GitHub\Powershell\Saved Help Files\help_get-ADUser.txt"
+Get-help *Export*CSV* -full
 
- Get-help *total* -full
+import-csv .\newusers.csv | New-ADUser
+import-csv "C:\Users\lgrisby\Downloads\GitHub\Powershell\Month of Lunches\Labs\newusers.csv" | New-ADUser
+
+"C:\Users\lgrisby\Downloads\GitHub\Powershell\Month of Lunches\Labs\ADUser-info-LGrisby.txt"
+
+Get-ADUser -Filter 'surname -like "*grisby"' | FT Name, SamAccountName, DistinguishedName -A
+Get-ADUser -Filter 'Name -like "*lewis"' | gm
+Get-ADUser -Filter 'surname -like "*lee"' | Out-File -FilePath "C:\Users\lgrisby\Downloads\GitHub\Powershell\Month of Lunches\Labs\Get-ADUser-info-.txt"
+
+# Copied from the book
+import-csv .\newusers.csv |
+select-object -property *,
+@{name='samAccountName';expression={$_.login}},
+@{label='Name';expression={$_.login}},
+@{n='Department';e={$_.Dept}} |
+New-ADUser
+
+#Testing
+Get-Help Get-ADComputer -Full
+
+# search for computer information on the Domain Controller
+Get-ADComputer "D-007" -Properties * | GM
+
+# search a specific OU for computers in that OU
+#this is all the computers in LKS11 OU
+Get-ADComputer -Filter * -searchbase "OU=LKS11,OU=LKS,OU=Desktops,OU=Workstations,DC=BART,DC=DOMAIN" | Out-File -FilePath "C:\Users\lgrisby\Downloads\GitHub\Powershell\Month of Lunches\Labs\ADComputer-info-D007.txt"
+
+# get a list of computer names
+#research the output object
+Get-ADComputer -Filter * -searchbase "OU=LKS11,OU=LKS,OU=Desktops,OU=Workstations,DC=BART,DC=DOMAIN" | Select-Object |GM
+#select only the name and save that to a CSV file
+Get-ADComputer -Filter * -searchbase "OU=LKS11,OU=LKS,OU=Desktops,OU=Workstations,DC=BART,DC=DOMAIN" | Select-Object name | Export-CSV -Path "C:\Users\lgrisby\Downloads\GitHub\Powershell\Month of Lunches\Labs\ADComputer-info-list of LKS11 computer names.txt"
